88063430cef77dc30dea204b24c462c49742b2ce
Albert S. <mail@quitesimple.org>
2024-07-07 22:20:55 +0200

    update copyright

--- adhocify.c
+++ adhocify.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2020 Albert S. <adhocify@quitesimple.org>
+ * Copyright (c) 2014-2024 Albert S. <adhocify@quitesimple.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
