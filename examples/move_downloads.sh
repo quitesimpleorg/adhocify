9a5e96f058d7ba87a5ac9829fb35e16d70f1c3e2
Albert S. <mail@quitesimple.org>
2025-06-22 11:49:34 +0200

    Update move_downloads.sh

--- examples/move_downloads.sh
+++ examples/move_downloads.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#moves all incoming files (e. g. downloads) to another directory. 
+#Hardlinks all incoming files (e. g. downloads) to another directory.
 #There, they will be put into subdirectories which are named after the current date (YYYYMMDD) to get some minimal automatic "organization".  
 #adhocify -d -m IN_CLOSE_WRITE -m IN_MOVED_TO -w /home/user/Downloads -w /home/user/other_dir /path/to/move_downloads.sh
 
@@ -12,11 +12,12 @@ today=$(date +%Y%m%d)
 TARGET_DIR="/target/dir/path"
 TODAY_DIR="$TARGET_DIR"/$today
 if [ ! -d "$TODAY_DIR" ] ; then
-mkdir "$TODAY_DIR"
-rm -f "$TARGET_DIR"/today
-ln -s "$TODAY_DIR" "$TARGET_DIR"/today
+	mkdir "$TODAY_DIR"
+	rm -f "$TARGET_DIR"/today
+	ln -s "$TODAY_DIR" "$TARGET_DIR"/today
 fi
-#You can also filter/grep the filename here and move certain patterns to other designated locations...
-mv "$INCOMING" "$TARGET_DIR"/$today/
+# Nowadays, some browsers don't like it when files are moved away immediately and might report download failure. Use hardlinks so they don't complain.
+# Alternatively, a sufficiently long enough "sleep" followed by "mv" might work
+ln "$INCOMING" "$TARGET_DIR"/$today/
 
 
